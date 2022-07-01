500ee0b519db5efa5e92d1fc8b28e7986ab3fa6c
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2022-07-01 08:39:48 +0200

    Changed asset url to izenemateak

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/ikastaroak/build/')
+    .setPublicPath('/izenemateak/build/')
     // only needed for CDN's or sub-directory deploy
     .setManifestKeyPrefix('build/')
 
